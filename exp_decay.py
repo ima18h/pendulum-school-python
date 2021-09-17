c12f35b19c50a7bc4d62ea5e6e5b7ce72e2bcd6a
Ane Ce Eksveen-Søbakken <anece@uio.no>
2021-09-17 12:19:09 +0200

    1c) Added func solve and edited __call__

--- exp_decay.py
+++ exp_decay.py
@@ -1,3 +1,7 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+
 
 class ExponentialDecay: 
     def __init__(self, a): 
@@ -5,8 +9,28 @@ class ExponentialDecay:
         if a < 0: 
             raise ValueError("The constant a cannot be negative")
 
-    def __call__(self, t, u):   # Makes the function f(t, u)
-        self.t = t
-        self.u = u
+    def __call__(self, t, u):   # Definerer funksjonen f(t, u)
         du_dt = -self.a*u
-        return du_dt     # Returns the derivative du/dt = -a*u
+        return du_dt            # Returnerer den deriverte du/dt = -a*u
+
+# Oppgave 1c)
+    def solve(self, u0, T, dt): # Beregner løsninger for 0 <= t <= T 
+        solution = solve_ivp(
+            ExponentialDecay(self.a), 
+            [0, T], 
+            [u0], 
+            t_eval = np.linspace(0, T, T//dt)
+        )
+        return solution.t, solution.y[0]
+
+
+# Tester solve metoden
+a = 0.05
+u0 = 8
+T = 100
+dt = 1
+decay_model = ExponentialDecay(a)
+t, u = decay_model.solve(u0, T, dt)
+
+plt.plot(t, u)
+plt.show()
