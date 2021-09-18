d1be84848ca197cc74781fb4677ab70af6ceaa52
Ane Ce Eksveen-Søbakken <anece@uio.no>
2021-09-18 20:14:46 +0200

    Added test when pendulum is at rest, added constant variable TOL

--- test_pendulum.py
+++ test_pendulum.py
@@ -1,15 +1,25 @@
 import numpy as np
 from pendulum import Pendulum
 
-def test_Pendulum():
-    t = 0  
+TOL = 1e-14
+
+def test_Pendulum_call():
     y = (np.pi/6, 0.15) 
     L = 2.7
-    tol = 1e-12
-    expected_dtheta = 0.15
-    expected_domega = -0.03320283660974
-    pendulum_object = Pendulum(L=L)
-    dtheta_dt, domega_dt = pendulum_object(t, y)
-    print(domega_dt, expected_domega)
-    assert abs(dtheta_dt-expected_dtheta) < tol
-    assert abs(domega_dt-expected_domega) < tol
+    expected_theta_deriv = 0.15
+    expected_omega_deriv = -1.816666666666667
+    pendulum = Pendulum(L=L)
+    theta_deriv, omega_deriv = pendulum(0, y)
+    assert abs(theta_deriv-expected_theta_deriv) < TOL
+    assert abs(omega_deriv-expected_omega_deriv) < TOL
+
+def test_Pendulum_at_rest():
+    y = (0, 0)
+    expected_theta_deriv = 0
+    expected_omega_deriv = 0
+    pendulum = Pendulum()
+    theta_deriv, omega_deriv = pendulum(0, y)
+    assert abs(theta_deriv-expected_theta_deriv) < TOL
+    assert abs(omega_deriv-expected_omega_deriv) < TOL
+
+
