66bc7dedbe8115e41104f13fb120484b94f46f57
Ane Ce Eksveen-Søbakken <anece@uio.no>
2021-09-18 19:35:08 +0200

    2a) Implemented class Pendulum with __init__ and __class__

--- pendulum.py
+++ pendulum.py
@@ -1,17 +1,22 @@
+import numpy as np
 
 # Oppgave 2a)
 class Pendulum: 
     def __init__(self, L=1, M=1, g=9.81):
-        """ 
+        """
         Tar inn parameterne L, M og g. Om noe annet ikke er gitt så er 
-        L = 1 m, M = 1 kg og g = 9.81 m/s^2 som standard verdi.
+        L = 1 m, M = 1 kg og g = 9.81 m/s^2 som standardverdi.
         """
         self.L = L
         self.M = M
         self.g = g
 
     def __call__(self, t, y):
+        """
+        Tar inn parameterne t og y som brukes i to funksjoner og
+        returnerer de deriverte (h.s. av ODE-systemene).
+        """
         theta, omega = y
-        
-        return
-
+        dtheta_dt = omega
+        domega_dt = -self.g/self.L*np.sin(theta)
+        return dtheta_dt, domega_dt
