3b6a5bf26c0538837f7d379f9af8237d816bc2bc
Ane Ce Eksveen-Søbakken <anece@uio.no>
2021-09-19 13:12:17 +0200

    Add method solve

--- pendulum.py
+++ pendulum.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.integrate import solve_ivp
 
 # Oppgave 2a)
 class Pendulum: 
@@ -20,3 +21,16 @@ class Pendulum:
         theta_deriv = omega
         omega_deriv = -self.g/self.L*np.sin(theta)
         return theta_deriv, omega_deriv
+
+    # Oppgave 2c)
+    def solve(self, y0, T, dt, angle="rad"):
+        """ Beregner løsninger av ODE-systemet for 0 <= t <= T """
+        if angle == "deg": 
+            angle = np.radians("deg")
+
+        solution = solve_ivp(
+            Pendulum(self.L, self.M, self.g), [0, T], [y0],
+            t_eval=np.linspace(0, T, T // dt)
+        )
+        self.solution_t, self.solution_y = solution.t, solution.y[0]
+        
