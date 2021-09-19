6aade492e1ab2b49f2285bb6a74867d47db08424
Ane Ce Eksveen-Søbakken <anece@uio.no>
2021-09-19 22:36:08 +0200

    Add tests for checking AssertionError and the arrays t, theta and omega are correct

--- test_pendulum.py
+++ test_pendulum.py
@@ -1,8 +1,10 @@
 import numpy as np
 from pendulum import Pendulum
+import pytest
 
 TOL = 1e-14
 
+# Oppgave 2b)
 def test_Pendulum_call():
     y = (np.pi/6, 0.15) 
     L = 2.7
@@ -22,4 +24,31 @@ def test_Pendulum_at_rest():
     assert abs(theta_deriv-expected_theta_deriv) < TOL
     assert abs(omega_deriv-expected_omega_deriv) < TOL
 
+# Oppgave 2e)
+def test_calling_private_attributes_raises_AssertionError():
+    pendulum = Pendulum()
+    with pytest.raises(AssertionError):
+        pendulum.t, pendulum.theta, pendulum.omega
+    
+    pendulum.solve()
+    pendulum.t, pendulum.theta, pendulum.omega
 
+def test_initial_condition_zero_gives_arrays_zero():
+    T = 10
+    dt = 1
+    pendulum = Pendulum()
+    pendulum.solve((0, 0), T, dt)
+    expected_theta = np.zeros_like(pendulum.theta)
+    expected_omega = np.zeros_like(pendulum.omega)
+
+    print(pendulum.t)
+
+    assert np.all(pendulum.theta == expected_theta)
+    assert np.all(pendulum.omega == expected_omega)
+    
+    # Denne delen av testen funker ikke. Spør om hjelp! 
+    """
+    for i in range(0, T, dt):
+        expected_t_values = i*dt
+        assert np.all(pendulum.t == expected_t_values)
+    """
