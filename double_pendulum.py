d7ac8e5dc7ecd0467712552b14e9514ca5c6ba38
ima18h <ima18h@gmail.com>
2021-09-19 19:29:33 +0200

    Add equations and such to call method

--- double_pendulum.py
+++ double_pendulum.py
@@ -7,4 +7,20 @@ class Pendulum:
         self._g = g
 
     def __call__(self, t, y):
-        pass
+        theta1, omega1, theta2, omega2 = y
+        dtheta1 = omega1
+        dtheta2 = omega2
+        delta_t = theta2 - theta1
+        domega1 = ((self._L1 * omega1**2 * np.sin(delta_t) * np.cos(delta_t))
+                   + (self._g * np.sin(theta2) * np.cos(delta_t))
+                   + (self._L2 * omega2**2 * np.sin(delta_t))
+                   - (2 * self._g * np.sin(theta1))) / ((2 * self._L1)
+                                                        - (self._L1 * np.cos(delta_t)**2))
+
+        domega2 = (-(self._L2 * omega2**2 * np.sin(delta_t) * np.cos(delta_t))
+                   + (2 * self._g * np.sin(theta1) * np.cos(delta_t))
+                   - (2 * self._L1 * omega1**2 * np.sin(delta_t))
+                   - (2 * self._g * np.sin(theta2))) / ((2 * self._L2)
+                                                        - (self._L2 * np.cos(delta_t)**2))
+        return dtheta1, domega1, dtheta2, domega2
+
