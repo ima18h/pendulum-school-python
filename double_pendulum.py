7526c03838624ae7480994afd4c1177e8ce2a858
ima18h <imadha@math.uio.no>
2021-09-19 19:54:39 +0200

    Add cartesian properties

--- double_pendulum.py
+++ double_pendulum.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.integrate import solve_ivp
 
 
 class DoublePendulum:
@@ -6,6 +7,8 @@ class DoublePendulum:
         self._L1 = L1
         self._L2 = L2
         self._g = g
+        self._t = None
+        self._theta1, self._omega, self._theta2, self._omega2 = None, None, None, None
 
     def __call__(self, t, y):
         theta1, omega1, theta2, omega2 = y
@@ -24,3 +27,40 @@ class DoublePendulum:
                    - (2 * self._g * np.sin(theta2))) / ((2 * self._L2)
                                                         - (self._L2 * np.cos(delta_t) ** 2))
         return dtheta1, domega1, dtheta2, domega2
+
+    def solve(self, y0, T, dt, angle="rad"):
+        """ Beregner løsninger av ODE-systemet for 0 <= t <= T """
+        if angle == "deg":
+            angle = np.radians("deg")
+
+        sol = solve_ivp(self, (0, T), y0, max_step=dt)
+        self._t = sol.t
+        self._theta1, self._omega, self._theta2, self._omega2 = sol.y[0], sol.y[1], sol.y[2], sol.y[3]
+
+    @property
+    def t(self):
+        return self._t
+
+    @property
+    def theta1(self):
+        return self._theta1
+
+    @property
+    def theta2(self):
+        return self._theta2
+
+    @property
+    def x1(self):
+        return self._L1 * np.sin(self.theta1)
+
+    @property
+    def y1(self):
+        return -self._L1 * np.cos(self.theta1)
+
+    @property
+    def x2(self):
+        return self.x1 + self._L2 * np.sin(self.theta2)
+
+    @property
+    def y2(self):
+        return self.y1 - self._L2 * np.cos(self.theta2)
